71957849f8e988e5419192e4fd01578f49344351
wahajahmad-cyber <wahajahmad.alnafi@gmail.com>
2025-08-24 09:37:18 +0500

    Add speed-insights for vercel-React

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// Import Vercel Analytics for React
+// Vercel Analytics for React
 import { Analytics } from '@vercel/analytics/react'
+
+// Vercel Speed Insights for React
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Analytics />  {/* Tracks page views automatically */}
+    <Analytics />       {/* Tracks page views automatically */}
+    <SpeedInsights />   {/* Tracks performance metrics automatically */}
   </React.StrictMode>
 )
